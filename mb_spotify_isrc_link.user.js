478e0f51399cfe6ac70528396221934632240b69
Adam James <atj@pulsewidth.org.uk>
2021-01-30 21:14:41 +0000

    mb_spotify_isrc_link: Remove jQuery dependency

--- mb_spotify_isrc_link.user.js
+++ mb_spotify_isrc_link.user.js
@@ -1,30 +1,69 @@
 // ==UserScript==
 // @name        Add Spotify ISRC link to release pages
-// @version     2021.1.30.1
+// @version     2021.1.30.2
 // @description Adds an "import ISRCs" link to release pages with a Spotify URL
 // @author      atj
 // @license     MIT; https://opensource.org/licenses/MIT
 // @namespace   https://github.com/atj/userscripts
 // @downloadURL https://raw.github.com/atj/userscripts/master/mb_spotify_isrc_link.user.js
 // @updateURL	https://raw.github.com/atj/userscripts/master/mb_spotify_isrc_link.user.js
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @match       *://*.musicbrainz.org/release/*
 // @grant       none
 // @run-at      document-idle
 // ==/UserScript==
 
-// prevent JQuery conflicts, see http://wiki.greasespot.net/@grant
-this.$ = this.jQuery = jQuery.noConflict(true);
+const SpotifyLinkRegexp = new RegExp(
+    '^https?://open.spotify.com/album/([0-9a-z]+)',
+    'i'
+);
 
-$(document).ready(function () {
-    let spotifyLink = $('#release-relationships a[href*="open.spotify.com"]')[0];
+function addImportIsrcsLink() {
+    const releaseRels = document.getElementById('release-relationships');
 
-    if (spotifyLink === undefined) {
+    if (!releaseRels) {
         return;
     }
 
-    let mbId = window.location.href.replace(/^.+\/release\/([-0-9a-f]{36}).*$/i, '$1');
-    let spotifyId = spotifyLink.href.replace(/^.+\/album\/([0-9a-z]+)/i, '$1');
-    let tatsumoUrl = `https://d.ontun.es?entity=album&id=${spotifyId}&attach=${mbId}`;
-    $(spotifyLink.nextElementSibling.nextSibling).after(` [<a href="${tatsumoUrl}" target="_blank">import ISRCs</a>]`);
-});
+    let spotifyLink;
+    let spotifyId;
+
+    for (const bdi of releaseRels.getElementsByTagName('bdi')) {
+        const matches = SpotifyLinkRegexp.exec(bdi.innerText);
+
+        if (matches) {
+            spotifyId = matches[1];
+            spotifyLink = bdi.parentElement;
+            break;
+        }
+    }
+
+    if (spotifyId === undefined) {
+        return;
+    }
+
+    const mbId = window.location.href.replace(
+        /^.+\/release\/([-0-9a-f]{36}).*$/i,
+        '$1'
+    );
+    let curElem = spotifyLink.nextElementSibling.nextSibling;
+    let elem = document.createTextNode(' [');
+
+    curElem = insertAfter(elem, curElem);
+    elem = document.createElement('a');
+    elem.target = '_blank';
+    elem.href = `https://d.ontun.es?entity=album&id=${spotifyId}&attach=${mbId}`;
+    elem.innerText = 'import ISRCs';
+    curElem = insertAfter(elem, curElem);
+    elem = document.createTextNode(']');
+    insertAfter(elem, curElem);
+}
+
+function insertAfter(elem, after) {
+    if (after.parentNode) {
+        after.parentNode.insertBefore(elem, after.nextSibling);
+    }
+
+    return elem;
+}
+
+window.setTimeout(addImportIsrcsLink, 250);
